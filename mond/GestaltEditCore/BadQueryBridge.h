1e7e45abeb8b651f8f3ecc67ed42e37854fb92a7
bibilabusensei <pang20130804@qq.com>
2026-08-12 08:17:47 +0800

    Import GestaltEdit BadQueryBridge header

---
+++ mond/GestaltEditCore/BadQueryBridge.h
@@ -0,0 +1,26 @@
+//
+//  BadQueryBridge.h
+//  GestaltEdit
+//
+//  Path-based ContainerManager query derived from forcequitOS/bad_query.
+//  Source: https://github.com/frs0n/GestaltEdit
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BadQueryLease : NSObject
+
+@property(nonatomic, copy, readonly) NSString *targetPath;
+@property(nonatomic, readonly, getter=isActive) BOOL active;
+
++ (nullable instancetype)leaseForPath:(NSString *)path
+                                error:(NSString * _Nullable * _Nullable)error;
+- (void)invalidate;
+
+@end
+
+FOUNDATION_EXPORT BOOL BadQueryBridgeAvailable(void);
+
+NS_ASSUME_NONNULL_END
